f7172279e71d54107b08894820465c288bcaadfc
Tyler Cady <tylercady07@gmail.com>
2024-10-19 12:26:32 -0400

    foldersssss

--- src/navigator_adaptor/src/navigator_adaptor.cpp
+++ src/navigator_adaptor/src/navigator_adaptor.cpp
@@ -1,11 +1,26 @@
-#include <cstdio>
+#include "bindings.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-auto main(int argc, char *argv[]) -> int
-{
-    static_cast<void>(argc);
-    static_cast<void>(argv);
+int main() {
+  printf("Initiating navigator module.\n");
+  init();
 
-    std::printf("hello world navigator_adaptor package\n");
+  printf("Setting led on!\n");
+  set_led(UserLed::Led1, true);
 
-    return 0;
+  printf("Temperature: %f\n", read_temp());
+  printf("Pressure: %f\n", read_pressure());
+
+  ADCData adc = read_adc_all();
+  printf("Reading ADC Channels: 1 = %f, 2 = %f, 3 = %f, 4 = %f\n",
+         adc.channel[0], adc.channel[1], adc.channel[2], adc.channel[3]);
+  printf("Data ADC Channels: 1 = %f\n", read_adc(AdcChannel::Ch1));
+
+  AxisData mag = read_mag();
+  printf("Magnetic field: X = %f, Y = %f, Z = %f\n", mag.x, mag.y, mag.z);
+
+  return 0;
 }
