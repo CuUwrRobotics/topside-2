3b51dbb98d118ac12f59cb543d38548bc60bfd5e
Tyler Cady <tylercady07@gmail.com>
2024-11-21 21:37:39 -0500

    motor_driver is a thing

--- src/rov/motor_driver/motor_driver.cpp
+++ src/rov/motor_driver/motor_driver.cpp
@@ -15,19 +15,30 @@ void recieve_coefficients(float& coeffs[6]) {
         coeffs[i] = data[i];
     }
 }
-    
+
+void drive_motors(float pwms[6]) {
+    for (int i = 0; i < 6; i++) {
+        bindings::set_pwm(static_cast<PwmChannel>(i + 1), pwms[i]);
+    }
+}
 
 int main()
 {
-    
+    init();
     // zmq receiver
     zmq::context_t context(1);
     zmq::socket_t receiver(context, ZMQ_PULL);
     receiver.bind("tcp://*:5555");
 
     float coeffs[6];
-    recieve_coefficients(coeffs);
-    bytes_to_pwm(coeffs);
+    float pwms[6];
+    while (true) {
+        recieve_coefficients(coeffs);
+        bytes_to_pwm(coeffs);
+        drive_motors(pwms);
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
     
     return 0;
 }
