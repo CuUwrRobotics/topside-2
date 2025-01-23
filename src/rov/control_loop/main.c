72067c06af6f000d3fca42674cf310f2f5bfdf4f
Tyler Cady <tylercady07@gmail.com>
2025-01-22 19:18:05 -0500

    control loop (PID) to some degree

---
+++ src/rov/control_loop/main.c
@@ -0,0 +1,28 @@
+#include "control_loop.h"
+
+int main()
+{
+    // Current simulation time
+    float t = 0;
+
+    // Iteration counter
+    int i = 0;
+
+    // Setpoint and output of the first control loop
+    float command1 = 0;
+    float stp1 = 100;
+    float z1 = 0;
+
+    // Setpoint and output of the second control loop
+    float command2 = 0;
+    float stp2 = 50;
+    float z2 = 0;
+
+    // PID controller parameters for the first control loop
+    struct PID pid1 = {1, 0.1, 5, 0.1, 1, TIME_STEP, 100, -100, 40, 0, 0, 0, 0, 0};
+
+    // Object parameters for the first control loop
+    struct ROV_Object rov1 = {1, 0.1, 100, -100, TIME_STEP, 0, 0};
+
+
+}
--- src/rov/control_loop/pid.h
+++ src/rov/control_loop/pid.h
@@ -1,7 +1,35 @@
-class pid {
-    public:
-        pid();
-        pid(float kp, float ki, float kd);
-        ~pid();
-        
+#pragma once
+
+struct PID
+{
+    float Kp;              /* Proportional gain constant */
+    float Ki;              /* Integral gain constant */
+    float Kd;              /* Derivative gain constant */
+    float Kaw;             /* Anti-windup gain constant */
+    float T_C;             /* Time constant for derivative filtering */
+    float T;               /* Time step */
+    float max;             /* Max command */
+    float min;             /* Min command */
+    float max_rate;        /* Max rate of change of the command */
+    float integral;        /* Integral term */
+    float err_prev;        /* Previous error */
+    float deriv_prev;      /* Previous derivative */
+    float command_sat_prev;/* Previous saturated command */
+    float command_prev;    /* Previous command */
 };
+
+void PID_Init(struct PID *pid, float Kp, float Ki, float Kd, float Kaw, float T_C, float T, float max, float min, float max_rate);
+float PID_Step(struct PID *pid, float measurement, float setpoint);
+
+struct ROV_Object
+{
+    float m;         /* ROV mass*/
+    float k;         /* Damping Constant */
+    float F_max;     /* Maximum force */
+    float F_min;     /* Minimum force */
+    float T;         /* Time step */
+    float v;         /* Velocity */
+    float z;         /* Position */
+};
+void ROV_Init(struct ROV_Object *rov, float m, float k, float F_max, float F_min, float T, float v, float z);
+void ROV_Step(struct ROV_Object *rov, float F, float T);
