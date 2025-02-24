1084ab93ce755898d568a040034ddd6476204753
Brendan Connelly <brendanjconnelly17@gmail.com>
2025-02-24 18:20:25 -0500

    header for sensor data

---
+++ src/rov/sensor_driver/include/sensors.h
@@ -0,0 +1,21 @@
+#pragma once
+
+struct sensor_data {
+    float x;
+    float y;
+    float z;
+    float v_x;
+    float v_y;
+    float v_z;
+    float a_x;
+    float a_y;
+    float a_z;
+    float yaw;
+    float pitch;
+    float roll;
+    float v_yaw;
+    float v_pitch;
+    float a_roll;
+};
+
+struct snesor_data *read_sensors();
