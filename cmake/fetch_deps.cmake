ff84f4bcb0bbee8190684d3a6e9a233c180e37f1
carykees98 <trainboy980@gmail.com>
2025-03-02 17:20:08 +0000

    added cppzmq as a dependency

--- cmake/fetch_deps.cmake
+++ cmake/fetch_deps.cmake
@@ -5,3 +5,9 @@ FetchContent_Declare(spdlog
     GIT_TAG v1.15.1
 )
 FetchContent_MakeAvailable(spdlog)
+
+FetchContent_Declare(cppzmq
+    GIT_REPOSITORY https://github.com/zeromq/cppzmq
+    GIT_TAG v4.10.0
+)
+FetchContent_MakeAvailable(cppzmq)
