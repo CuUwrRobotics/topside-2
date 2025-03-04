2fdb7284afaa60cd6cb22bd771c4a7de48e26c1d
carykees98 <trainboy980@gmail.com>
2025-03-04 00:29:31 +0000

    logging after attempting build

--- build.py
+++ build.py
@@ -4,11 +4,13 @@ import logging
 import subprocess
 import os
 
+
 def build_topside() -> bool:
     # Configure build
     currentDirectory = os.getcwd()
 
-    logging.info(f"[Topside] Setting working dir to {currentDirectory}/lib/navigator-lib")
+    logging.info(
+        f"[Topside] Setting working dir to {currentDirectory}/lib/navigator-lib")
     os.chdir(currentDirectory + "/lib/navigator-lib")
 
     logging.info("[Topside] Building navigator-lib")
@@ -18,51 +20,61 @@ def build_topside() -> bool:
         logging.error("[Topside] Failed to build navigator-lib")
         print(cargo.stderr)
         return False
-    
+
     logging.info("[Topside] Successfully built navigator-lib!")
 
     logging.info(f"[Topside] Setting working dir to {currentDirectory}")
     os.chdir(currentDirectory)
 
     logging.info("[Topside] Configuring CMake build for topside")
-    cmake_configure = subprocess.run(["cmake", "-B", "build", "-G", "Ninja"], capture_output=True)
+    cmake_configure = subprocess.run(
+        ["cmake", "-B", "build", "-G", "Ninja"], capture_output=True)
 
     if (cmake_configure.returncode != os.EX_OK):
         logging.error("[Topside] Failed to configure CMake")
         print(cmake_configure.stderr)
         return False
-    
+
     logging.info("[Topside] Successfully configured CMake!")
 
     logging.info("[Topside] Building CMake target: topside")
-    cmake_build = subprocess.run(["cmake", "--build", "build", "--target", "topside"], capture_output=True)
+    cmake_build = subprocess.run(
+        ["cmake", "--build", "build", "--target", "topside"], capture_output=True)
 
     if (cmake_build.returncode != os.EX_OK):
         logging.error("[Topside] Failed to build target: topside")
         print(cmake_build.stderr)
         return False
-        
+
     logging.info("[Topside] Successfully built target: topside!")
 
     return True
 
+
 def build_rov() -> bool:
     return False
+
 
 def main():
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s.%(msecs)03d] [%(levelname)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
-)
+    )
 
     logging.info('Building topside')
     if not build_topside():
         logging.error('Failed to build topside. Terminating build')
+        exit(os.EX_TEMPFAIL)
+
+    logging.info('Successfully built topside!')
 
     logging.info('Building rov')
     if not build_rov():
         logging.error('Failed to build rov. Terminating build')
+        exit(os.EX_TEMPFAIL)
+
+    logging.info('Successfully built rov!')
 
 
 if __name__ == "__main__":
