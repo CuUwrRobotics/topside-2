6524532dba3bbbb1f24f9796edc617450b92841f
Brendan Connelly <brendanjconnelly17@gmail.com>
2025-03-12 18:32:39 -0400

    added sensor math a littel bit

--- src/rov/control_loop/src/state.cpp
+++ src/rov/control_loop/src/state.cpp
@@ -28,21 +28,42 @@ struct sensor_data *get_sensors()
     return data;
 }
 
+struct computed_state *prev_state = NULL;
 struct computed_state *get_state()
 {
     float delta = get_delta_t();
     struct sensor_data *data = get_sensors();
     struct computed_state *state = new struct computed_state;
+    
+    /* x and y are based on integration of the accelerometer data
+       z still is, but we should switch it to being based on the pressure */
     state->a_x = data->a_x;
     state->a_y = data->a_y;
     state->a_z = data->a_z;
+    state->v_x += state->a_x * delta;
+    state->v_y += state->a_y * delta;
+    state->v_z += state->a_z * delta;
+    state->x += state->v_x * delta;
+    state->y += state->v_y * delta;
+    state->z += state->v_z * delta;
 
+    /* angular positions are based on integration, but angular acceleration is
+       based on deriving the angular velocities */
     state->v_yaw = data->v_yaw;
     state->v_pitch = data->v_pitch;
     state->v_roll = data->v_roll;
+    state->yaw += state->v_yaw * delta;
+    state->pitch += state->v_pitch * delta;
+    state->roll += state->v_roll * delta; 
+    state->a_yaw = (state->v_yaw - prev_state->v_yaw) / delta;
+    state->a_pitch = (state->v_pitch - prev_state->v_pitch) / delta;
+    state->a_roll = (state->v_roll - prev_state->v_roll) / delta;
+    
     
     state->pressure = data->pressure;
 
     delete data;
+    if(prev_state != NULL) delete prev_state;
+    prev_state = state;
     return state;
 }
