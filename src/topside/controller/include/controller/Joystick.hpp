889b72bcedb60d5170534029723c08c2b8aa3cc4
carykees98 <trainboy980@gmail.com>
2025-03-12 22:48:06 +0000

    added INIT_BUTTON and INIT_AXIS events

--- src/topside/controller/include/controller/Joystick.hpp
+++ src/topside/controller/include/controller/Joystick.hpp
@@ -11,14 +11,16 @@ namespace cuuwr::topside::controller
 {
 enum class JoystickEventType : std::uint8_t
 {
-    BUTTON = 0x01,
-    AXIS   = 0x02,
-    INIT   = 0x80,
+    BUTTON      = 0x01,
+    AXIS        = 0x02,
+    INIT        = 0x80,
+    INIT_BUTTON = INIT | BUTTON,
+    INIT_AXIS   = INIT | AXIS,
 };
 
 struct JoystickEvent
 {
-    std::uint32_t     time;
+    std::uint32_t     time;     /* event timestamp in milliseconds */
     std::int16_t      value;    /* value */
     JoystickEventType type;     /* event type */
     std::uint8_t      buttonID; /* axis/button number */
