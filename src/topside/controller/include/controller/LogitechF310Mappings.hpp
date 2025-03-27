57e0812a236adb8c9d4714ae8193a296590385f2
carykees98 <trainboy980@gmail.com>
2025-03-27 22:39:29 +0000

    updated mappings to reflect change from joydev to evdev

--- src/topside/controller/include/controller/LogitechF310Mappings.hpp
+++ src/topside/controller/include/controller/LogitechF310Mappings.hpp
@@ -9,29 +9,46 @@
 
 namespace cuuwr::topside::controller
 {
-enum class ControllerButtons : std::uint8_t
+/**
+ * @brief Button mappings determined through testing. Mappings provided by
+ * <linux/input-event-codes.h> are incorrect for this controller
+ *
+ */
+enum class ControllerButtons : std::uint16_t
 {
-    X              = 0x0,
-    A              = 0x1,
-    B              = 0x2,
-    Y              = 0x3,
-    LEFT_BUMPER    = 0x4,
-    RIGHT_BUMPER   = 0x5,
-    LEFT_TRIGGER   = 0x6,
-    RIGHT_TRIGGER  = 0x7,
-    BACK           = 0x8,
-    START          = 0x9,
-    LEFT_JOYSICK   = 0xA,
-    RIGHT_JOYSTICK = 0xB,
+    X              = 0x120,
+    A              = 0x121,
+    B              = 0x122,
+    Y              = 0x123,
+    LEFT_BUMPER    = 0x124,
+    RIGHT_BUMPER   = 0x125,
+    LEFT_TRIGGER   = 0x126,
+    RIGHT_TRIGGER  = 0x127,
+    BACK           = 0x128,
+    START          = 0x129,
+    LEFT_JOYSICK   = 0x12A,
+    RIGHT_JOYSTICK = 0x12B,
 };
 
-enum class ControllerAxes : std::uint8_t
+enum class ControllerAxes : std::uint16_t
 {
     LEFT_JOYSTICK_X  = 0x0,
     LEFT_JOYSTICK_Y  = 0x1,
     RIGHT_JOYSTICK_X = 0x2,
-    RIGHT_JOYSTICK_Y = 0x3,
-    D_PAD_X          = 0x4,
-    D_PAD_Y          = 0x5,
+    RIGHT_JOYSTICK_Y = 0x5,
+    D_PAD_X          = 0x16,
+    D_PAD_Y          = 0x17,
+};
+
+enum class AxisExtrema : std::int32_t
+{
+    JOYSTICK_UP    = 0,
+    JOYSTICK_DOWN  = 255,
+    JOYSTICK_LEFT  = 0,
+    JOYSTICK_RIGHT = 255,
+    DPAD_UP        = -1,
+    DPAD_DOWN      = 1,
+    DPAD_LEFT      = -1,
+    DPAD_RIGHT     = 1,
 };
 } // namespace cuuwr::topside::controller
