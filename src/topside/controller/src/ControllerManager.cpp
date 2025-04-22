0ab18780e6a50a7ffb5f8574576fa977baaa74ed
carykees98 <trainboy980@gmail.com>
2025-04-22 01:19:49 -0400

    used unimplemented in ControllerManager

--- src/topside/controller/src/ControllerManager.cpp
+++ src/topside/controller/src/ControllerManager.cpp
@@ -14,6 +14,9 @@
 #include <spdlog/spdlog.h>
 #include <zmq.hpp>
 
+// Project Includes
+#include <common/Unimplemented.hpp>
+
 namespace cuuwr::topside::controller
 {
 ControllerManager::ControllerManager()
@@ -110,107 +113,145 @@ auto ControllerManager::InputVisitor::operator()(const ButtonInput& input)
 auto ControllerManager::ButtonHandlers::a(const bool isPressed) -> void
 {
     spdlog::trace("A button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("A button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::b(const bool isPressed) -> void
 {
     spdlog::trace("B button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("B button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::x(const bool isPressed) -> void
 {
     spdlog::trace("X button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("X button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::y(const bool isPressed) -> void
 {
     spdlog::trace("Y button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Y button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::left_bumper(const bool isPressed)
     -> void
 {
     spdlog::trace("Left bumper {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Left bumper has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::right_bumper(const bool isPressed)
     -> void
 {
     spdlog::trace("Right bumper {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Right bumper has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::back(const bool isPressed) -> void
 {
     spdlog::trace("Back button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Back button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::start(const bool isPressed) -> void
 {
     spdlog::trace("Start button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Start button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::logo_button(const bool isPressed)
     -> void
 {
     spdlog::trace("Logo button {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Logo button has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::left_joystick(const bool isPressed)
     -> void
 {
     spdlog::trace("Left stick {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Left stick has no mapping");
 }
 
 auto ControllerManager::ButtonHandlers::right_joystick(const bool isPressed)
     -> void
 {
     spdlog::trace("Right stick {}pressed", (isPressed ? "" : "un"));
+
+    common::unimplemented("Right stick has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::left_joystick_x(const std::int16_t value)
     -> void
 {
     spdlog::trace("Left x axis moved");
+
+    common::unimplemented("Left x axis has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::left_joystick_y(const std::int16_t value)
     -> void
 {
     spdlog::trace("Left y axis moved");
+
+    common::unimplemented("Left y axis has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::left_trigger(const std::int16_t value)
     -> void
 {
     spdlog::trace("Left trigger moved");
+
+    common::unimplemented("Left trigger has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::right_joystick_x(const std::int16_t value)
     -> void
 {
     spdlog::trace("Right x axis moved");
+
+    common::unimplemented("Right x axis has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::right_joystick_y(const std::int16_t value)
     -> void
 {
     spdlog::trace("Right y axis moved");
+
+    common::unimplemented("Right y axis has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::right_trigger(const std::int16_t value)
     -> void
 {
     spdlog::trace("Right trigger moved");
+
+    common::unimplemented("Right trigger has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::d_pad_x(const std::int16_t value) -> void
 {
     spdlog::trace("D-pad x moved");
+
+    common::unimplemented("D-pad x has no mapping");
 }
 
 auto ControllerManager::AxisHandlers::d_pad_y(const std::int16_t value) -> void
 {
     spdlog::trace("D-pad y moved");
+
+    common::unimplemented("D-pad y has no mapping");
 }
 
 auto ControllerManager::run() -> void
